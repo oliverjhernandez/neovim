90b4b1cdadeaaa88aee0dbc8d26ad0e752230e8e
oliverjhernandez <oliverjhernandez@gmail.com>
2023-07-18 15:30:40 -0500

    Added tokyonight colorscheme

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {"sainnhe/gruvbox-material", enabled = true},
+    {"folke/tokyonight.nvim", enabled = true}
+}
