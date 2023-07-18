27edcd63a13173f6ad939b7b1be6839ea537a23b
oliverjhernandez <oliverjhernandez@gmail.com>
2023-07-18 15:25:43 -0500

    Added terraform plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,14 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {"juliosueiras/terraform-lsp", enabled = true},
+    {"hashivim/vim-terraform", enabled = true}
 }
